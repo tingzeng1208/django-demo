5bc0a1f1535e838bd3aa4dd3f3d9ab5317b8dd1d
christ.airey <ting.zeng@tag.com>
2023-12-08 17:30:12 -0500

    connect db to AWS and add templates

--- django-demo/demo/demo/settings.py
+++ django-demo/demo/demo/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+IMAGE_ROOT = f"{Path(__file__).resolve().parent.parent}/images"
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,12 +77,24 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo',  # Database name
+        'USER': 'newuser',  # RDS username
+        'PASSWORD': 'password',  # RDS password
+        'HOST': '3.137.152.114',  # RDS endpoint
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
--- django-demo/demo/hubzone/views.py
+++ django-demo/demo/hubzone/views.py
@@ -3,10 +3,25 @@ from django.http import HttpResponse
 
 # Create your views here.ll
 
+users = [
+  {
+    'id': '1',
+    'name': 'John Doe'
+  },
+  {
+    'id': '2',
+    'name': 'Jane Doe'
+  },
+  {
+    'id': '3',
+    'name': 'Steve Smith'
+  }
+]
+
 def home(request):
     # return HttpResponse('Home Page')
     return render(request, 'home.html')
 
 def hubzone(request):
     # return HttpResponse('Hubzone Page')
-    return render(request, 'hubzone.html')
+    return render(request, 'hubzone.html', {'users': users})
